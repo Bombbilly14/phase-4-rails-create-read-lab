b3e1db7da72234e2796a239711e3802a6a4db234
William German <william.j.german2@gmail.com>
2023-01-10 08:43:48 -0800

    commit tree 90983a84baffe6c057b355ff6598ede3d2aa9fa9

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,18 @@
+class PlantsController < ApplicationController
+  # wrap_parameters format: []
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plants = Plant.find_by(id: params[:id])
+    render json: plants
+
+  end
+
+  def create
+    plant = Plant.create(name: params[:name], image: params[:image], price: params[:price])
+    render json: plant, status: :created
+  end
+end
